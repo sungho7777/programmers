9431fcb4a475f75010974cd0fb65dfb5b26ca415
Sung Ho Park <sunghopark@Sungui-MacBookPro.local>
2022-04-18 21:02:42 +0900

    lv1 failureRate

--- js/lv1_Solution.js
+++ js/lv1_Solution.js
@@ -1,4 +1,5 @@
 function init(){
+  /*
   //  크레인 인형 뽑기게임
   fn_catchUpGame(
     [
@@ -18,6 +19,9 @@ function init(){
   fn_newIdRecommendation("");
   fn_newIdRecommendation("123_.def");
   fn_newIdRecommendation("abcdefghijklmn.p");
+ */
+  // 실패율 구하기
+  fn_failureRate(5, [2, 1, 2, 6, 2, 4, 3, 3]);
 }
 
 //
@@ -101,4 +105,40 @@ function fn_newIdRecommendation(new_id) {
     }
 
     return answer;
+}
+
+// 실패율 구하기
+function fn_failureRate(N, stages){
+  let arr = [];
+  let totalNum = stages.length;
+  for (let i = 1; i <= N; i++){
+    // 현재 단계에서 실패한 유저수를 계산
+    // 현재까지의 누적 도전자수를 나누어 실패율 계산
+    // 현재까지의 누적 도전자수에 실패한 유저수 빼기
+    let stageNum = stages.filter(ele => ele == i).length
+    let failRatio = 0;
+
+    if (stageNum === 0){
+      failRatio = 0;
+    }else{
+      failRatio = (stageNum) / totalNum;
+    }
+    totalNum -= stageNum;
+    arr.push({idx:i,ratio:failRatio});
+  }
+  // 실패율 내림차순으로 정렬하고 실패율이 같다면 stage 오름차순으로 정렬
+  arr.sort((a,b) => {
+    if(a.ratio > b.ratio){
+      return -1;
+    }else if (a.ratio < b.ratio){
+      return 1;
+    }else{
+      if(a.idx > b.idx){
+        return 1;
+      }else{
+        return -1;
+      }
+    }
+  })
+  return arr.map(ele => ele.idx);
 }
